f27029bfd9227c9937a52076c9c0d0baa1880894
Hanson Yang <358831910@qq.com>
2016-05-04 10:38:47 +0800

    第一版

---
+++ HansonThink/HansonThink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HansonThink
+//
+//  Created by HansonYang on 16/5/4.
+//  Copyright © 2016年 HansonSpeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HansonThink/HansonThink/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HansonThink
+//
+//  Created by HansonYang on 16/5/4.
+//  Copyright © 2016年 HansonSpeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
